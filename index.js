b3f8782823e53fea570ef6c2cb6170c6fd5ea6c2
mrdarboe <gsdarboe1@gmail.com>
2024-08-12 08:18:22 +0000

    logic pseudo

--- index.js
+++ index.js
@@ -9,7 +9,52 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
+/**
+ * Connect DB
+ * Create Schemas & Models for
+ *  User
+ *    username
+ *    id
+ *  Exercise
+ *    User.username
+ *    description
+ *    duration
+ *    date toDateString()
+ *    id
+ *    
+ *  POST /api/users
+ *    create new User username
+ *    res.json({username, id})
+ * 
+ *  GET /api/users
+ *    res.json([{all_users}])
+ * 
+ *  POST /api/users/:_id/exercises
+ *    create new Exercise
+ *      description
+ *      duration
+ *      date ? date : current date
+ *      create save() new Log **LOGIC
+ *      res.json({user, exercise})
+ * 
+ *  GET /api/users/:_id/logs
+ *    req.params.from yyyy-mm-dd
+ *    req.params.to yyyy-mm-dd
+ *    req.params.limit 1
+ * 
+ *    const full_log = findOne(Logs.username = username)
+ *    res.json({full_log})
+ *      User.name
+ *      count array.length + 1
+ *      id
+ *      log: [{
+ *        Exercise.description
+ *        Exercise.duration
+ *        Exercise.date  
+ *      }]
+ *  
+ *  
+ */
 
 
 
